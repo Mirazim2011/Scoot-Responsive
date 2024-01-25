8bab0d6e531875a132f20254269d4201c93d0a99
Mirazim2011 <mirazimmirzakarimov@gmail.com>
2024-01-25 07:12:20 +0500

    add footer section

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// elBtn = document.querySelector('.site-header__main-burger-btn');
+// elList = document.querySelector('.site-header__main-list');
+
+// elBtn.addEventListener('click',function () {
+//     elList.classList.toggle('menu-open')
+// });
